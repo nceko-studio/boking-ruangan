360217220b4528d750416b3351631f0825b743f8
nceko <nc.ekohidayat@gmail.com>
2023-06-19 18:20:00 +0800

    [+] Test Templating

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('template/public/home_view/header');
+		$this->load->view('public/home_view/index');
+		$this->load->view('template/public/home_view/footer');
 	}
 }
