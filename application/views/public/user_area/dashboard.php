d0ec1ed888fa4af2ab9306f47a0fee7f3edd3a4c
nceko <nc.ekohidayat@gmail.com>
2023-06-21 15:58:05 +0800

    [+] Fix SideBar User

--- application/views/public/user_area/dashboard.php
+++ application/views/public/user_area/dashboard.php
@@ -16,7 +16,7 @@
             					</div>
             					<div class="col-sm-5 text-center text-sm-left">
             						<div class="card-body pb-0 px-0 px-md-4">
-            							<img src="<?= base_url('public/img/illustrations/man-with-laptop-light.png') ?>" height="140" alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png" data-app-light-img="illustrations/man-with-laptop-light.png" />
+            							<img src="<?= base_url('assets/public/img/illustrations/man-with-laptop-light.png') ?>" height="140" alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png" data-app-light-img="illustrations/man-with-laptop-light.png" />
             						</div>
             					</div>
             				</div>
