ed03879b0406a0ddac0176e0dd4cdb6a644ce041
nceko <nc.ekohidayat@gmail.com>
2023-06-21 15:37:09 +0800

    [+] Fix View Home

--- application/views/public/home_view/index.php
+++ application/views/public/home_view/index.php
@@ -1,1 +1,50 @@
-Home
+<!-- Contact Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h4 class="section-title bg-white text-center text-primary px-3 mb-5">Informasi Ruangan</h4>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-12 col-md-2 wow fadeInUp" data-wow-delay="0.1s">
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Lantai</th>
+                            <th>Nama Ruangan</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Lantai I</td>
+                            <td>UGD</td>
+                            <td class="">Tersedia</td>
+                            <td><button class="btn btn-success">Detail</i></button></td>
+
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Lantai I</td>
+                            <td>Apotik</td>
+                            <td>Buka</td>
+                            <td><button class="btn btn-success">Detail</i></button></td>
+
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Lantai I</td>
+                            <td>Poli Anak</td>
+                            <td>Buka</td>
+                            <td><button class="btn btn-success">Detail</i></button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Contact End -->
--- application/views/template/public/home_view/header.php
+++ application/views/template/public/home_view/header.php
@@ -43,5 +43,4 @@
 
 <body>
 	<!-- Layout wrapper -->
-	<div class="layout-wrapper layout-content-navbar">
-		<div class="layout-container">
+	<div class="layout-wrapper">
