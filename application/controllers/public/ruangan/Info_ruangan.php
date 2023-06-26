5a9136302247df7447e988e56e66e132ef5605b4
nceko <nc.ekohidayat@gmail.com>
2023-06-27 04:24:59 +0800

    [+] Fix Landing Page

--- application/controllers/public/ruangan/Info_ruangan.php
+++ application/controllers/public/ruangan/Info_ruangan.php
@@ -6,15 +6,11 @@ class Info_ruangan extends CI_Controller
     public function index()
     {
         $data['title'] = 'Informasi Ruangan';
+        $data['ruangan'] = $this->M_ruangan->AllRuangan();
         $data['info_ruangan'] = $this->M_ruangan->getRuangan();
         $this->load->view('template/public/home_view/header', $data);
         $this->load->view('template/public/home_view/navbar', $data);
         $this->load->view('public/home_view/index', $data);
         $this->load->view('template/public/home_view/footer', $data);
-    }
-
-    public function pesan()
-    {
-        echo "SELAMAT ANDA SUDAH MEMESAN RUANGAN INI";
     }
 }
